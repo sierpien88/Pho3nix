889ca95c1f253c1676d1e9a5b7e662fe9be1bc1d
sierpien <stepnikagata@gmail.com>
2021-08-11 21:52:19 +0200

    numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ $(function() {
         }
         // Add replaced image's classes to the new SVG
         if (typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', `${imgClass}replaced-svg`);
+            $svg = $svg.attr('class', `${imgClass} replaced-svg`);
         }
 
         // Remove any invalid XML tags as per http://validator.w3.org
