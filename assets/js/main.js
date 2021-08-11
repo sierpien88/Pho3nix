f20cba5c5b68683af38446645b31e405bc2dd6aa
sierpien <stepnikagata@gmail.com>
2021-08-11 18:37:50 +0200

    Gallery and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ $('#all-initiatives').slick({
 $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
       //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
       var i = (currentSlide ? currentSlide : 0) + 1;
-      $status.html( '<span class="current_slide">' + i + '</span> / <span class="total_slides"> ' + slick.slideCount + '</span>');
+      $status.html( '<span class="current_slide">' + i + '</span> <span class="divider">/</span> <span class="total_slides"> ' + slick.slideCount + '</span>');
   });
 
 
@@ -83,7 +83,7 @@ infinite: true,
 centerPadding: '40px',
 slidesToShow: 1,
 dots: false,
-arrows: false
+arrows: true
 });
   
 });
